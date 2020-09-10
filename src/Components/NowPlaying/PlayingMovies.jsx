c745f44ee13a14407f40ff9e969bd7af0ea59762
SalmanShaik2668 <salmans@thoughtworks.com>
2020-09-10 20:29:42 +0530

    added material icons for arrows

--- src/Components/NowPlaying/PlayingMovies.jsx
+++ src/Components/NowPlaying/PlayingMovies.jsx
@@ -1,13 +1,16 @@
+import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
 import React from "react";
 import ScrollMenu from 'react-horizontal-scrolling-menu';
 import {createPosters} from "../../lib/helper";
 
 const Arrow = ({text, className}) => <div className={className}>{text}</div>;
-const ArrowLeft = Arrow({text: '<', className: 'arrow-prev'});
-const ArrowRight = Arrow({text: '>', className: 'arrow-next'});
 
-export const PlayingMovies = ({data,setCurrentShowId,setCurrentShowType,setHomePageLoaded}) => {
-    let menu = createPosters(data,setCurrentShowId,setCurrentShowType,setHomePageLoaded);
+const ArrowLeft = Arrow({text: <ArrowBackIos className="arrow"/>, className: 'arrow-prev'});
+const ArrowRight = Arrow({text: <ArrowForwardIos className="arrow"/>, className: 'arrow-next'});
+
+export const PlayingMovies = ({data, setCurrentShowId, setCurrentShowType, setHomePageLoaded}) => {
+    let menu = createPosters(data, setCurrentShowId, setCurrentShowType, setHomePageLoaded);
     return <section className="playing_movies">
         <ScrollMenu
             data={menu}
