9a49bcfbe409f4d8f1e49cae5a274354a60c1501
SalmanShaik2668 <salmans@thoughtworks.com>
2020-11-02 19:09:38 +0530

    Indication when copied

--- src/Components/LandingPage/Main/Show/Actions/ShareComponent.jsx
+++ src/Components/LandingPage/Main/Show/Actions/ShareComponent.jsx
@@ -1,15 +1,19 @@
 import Tooltip from "@material-ui/core/Tooltip";
 import Share from "@material-ui/icons/ShareTwoTone";
-import React from "react";
+import React, {useState} from "react";
 
-export const ShareComponent = ({ id, currentShowType, navigator }) => {
+export const ShareComponent = ({id, currentShowType, navigator}) => {
+  const [isClicked, setIsClicked] = useState(false);
+
   const copyUrl = () => {
     const showUrl = `${window.location.href}?showId=${id}&showType=${currentShowType}`;
     navigator.clipboard.writeText(showUrl).catch((e) => new TypeError(e));
+    setIsClicked(true);
+    setTimeout(() => setIsClicked(false), 3000);
   };
   return (
-    <Tooltip title={"Copy Url"}>
-      <Share className="share" onClick={copyUrl} data-testid="share" />
+    <Tooltip title={!!isClicked ? "Url Copied" : "Copy Url"}>
+      <Share className={`share ${!!isClicked ? "share_clicked" : ""}`} onClick={copyUrl} data-testid="share"/>
     </Tooltip>
   );
 };
