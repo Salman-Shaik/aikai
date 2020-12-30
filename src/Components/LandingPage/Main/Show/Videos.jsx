de8d6ef8767620ca9aab5b399b692d7854469727
SalmanShaik2668 <salmans@thoughtworks.com>
2020-12-30 21:09:14 +0530

    changes videos

--- src/Components/LandingPage/Main/Show/Videos.jsx
+++ src/Components/LandingPage/Main/Show/Videos.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import YouTube from "react-youtube";
 import "../../../../css/Videos.css";
-import { fetchVideoId } from "../../../../lib/showNetworkCalls";
-import { Spinner } from "../../../Spinner";
+import {fetchVideoId} from "../../../../lib/showNetworkCalls";
+import {Spinner} from "../../../Spinner";
 
-export const Videos = ({ info, homepageLoaded, setHomepageLoaded }) => {
+export const Videos = ({info, homepageLoaded, setHomepageLoaded}) => {
   const [videoIds, setVideoIds] = useState([]);
   const title = info.name || info.title;
 
@@ -14,46 +14,29 @@ export const Videos = ({ info, homepageLoaded, setHomepageLoaded }) => {
   };
 
   const subOpts = {
-    height: "230",
-    width: "325",
+    height: "470",
+    width: "670",
   };
 
   useEffect(() => {
+    //TODO: remove in develop
+    setHomepageLoaded(false);
     fetchVideoId(title, setVideoIds, setHomepageLoaded);
   }, [title, setHomepageLoaded]);
 
   return !homepageLoaded ? (
-    <Spinner loaded={homepageLoaded} />
+    <Spinner loaded={homepageLoaded}/>
   ) : (
     <section className="trailer_section">
-      <h3 className="trailer_title">Video</h3>
+      <h3 className="trailer_title">Videos</h3>
       <section className="videos">
-        <YouTube videoId={videoIds[0]} opts={opts} className="trailer_video" />
-        <div className="vertical-line" />
-        <section className="video_sub">
-          <YouTube
-            videoId={videoIds[1]}
-            opts={subOpts}
-            className="trailer_video"
-          />
-          <YouTube
-            videoId={videoIds[3]}
-            opts={subOpts}
-            className="trailer_video"
-          />
-        </section>
-        <section className="video_sub">
-          <YouTube
-            videoId={videoIds[2]}
-            opts={subOpts}
-            className="trailer_video"
-          />
-          <YouTube
-            videoId={videoIds[4]}
-            opts={subOpts}
-            className="trailer_video"
-          />
-        </section>
+        <YouTube videoId={videoIds[0]} opts={opts} className="trailer_video"/>
+        <div className="vertical-line"/>
+        <YouTube
+          videoId={videoIds[1]}
+          opts={subOpts}
+          className="trailer_video"
+        />
       </section>
     </section>
   );
