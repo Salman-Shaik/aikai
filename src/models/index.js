5f9a0a133983c5d2ceeafbc69f772ed160eb0b8e
SalmanShaik2668 <salmans@thoughtworks.com>
2021-04-01 10:54:20 +0530

    enabling ssl for sequalize

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const user = require("./User");
 const dbUrl = process.env.DATABASE_URL;
-console.log(`DB URL: ${dbUrl}`)
-const sequelize = new Sequelize(dbUrl);
+console.log(`DB URL: ${dbUrl}`);
+const sequelize = new Sequelize(dbUrl, { ssl: true });
 
 const models = {
   User: user(sequelize, DataTypes),
