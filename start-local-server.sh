a27593cff417e996bdf6d4e330e36df66e9b85f5
SalmanShaik2668 <salmans@thoughtworks.com>
2021-05-28 19:12:01 +0530

    Fixed id from url

--- start-local-server.sh
+++ start-local-server.sh
@@ -3,5 +3,5 @@
 export DATABASE_URL='postgres://localhost:5432/aikai'
 export TMDB_API_KEY='8f38dc176aea0ef9cbb167f50a8fc9b2'
 export YOUTUBE_API_KEY='blah'
-pg_ctl -D /usr/local/var/postgres start
+pg_ctl -D /usr/local/var/postgres restart
 prettier --write src/ && npm run build && npm start
